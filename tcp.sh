b0f9b93e72f97400b2b1d2ed4ca4d32233cd0e7f
9sr <30464098+9sr@users.noreply.github.com>
2018-01-26 09:44:57 +0800

    Update tcp.sh

--- tcp.sh
+++ tcp.sh
@@ -6,12 +6,12 @@ export PATH
 #	System Required: CentOS 6+,Debian7+,Ubuntu12+
 #	Description: BBR+BBR魔改版+Lotserver
 #	Version: 1.0.6
-#	Author: 千影
-#	Blog: https://www.94ish.me/
+#	Author: jiess
+#	Blog: https://www.josse.cn/
 #=================================================
 
 sh_ver="1.0.6"
-github="raw.githubusercontent.com/chiakge/Linux-NetSpeed/master"
+github="raw.githubusercontent.com/9sr/NetSpeed/master/"
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
